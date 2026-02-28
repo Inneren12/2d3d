ba8b568a9abc2854519cbed836ed5cc7b0d72da1
Inneren12 <chulkov.oleksii.can@gmail.com>
2026-02-28 06:02:56 -0500

    2

--- core/drawing2d/build.gradle.kts
+++ core/drawing2d/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 dependencies {
     implementation(project(":core:domain"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
